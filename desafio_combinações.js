6d2724697fe3ca7b788836fe9aea516e20f04aa7
João Arthur Silva <126902720+JoaoArthur25@users.noreply.github.com>
2023-04-22 02:07:38 -0300

    Add files via upload

---
+++ desafio_combinações.js
@@ -0,0 +1,27 @@
+let numeros = [-11, 13, -17, 19, -23, 29, -31, 37, 41, 43];
+let combinacoesTotais = [];
+
+function encontrarCombinacoes(somaDesejada, indiceAtual, combinacaoAtual) {
+  if (somaDesejada === 0) {
+    // combinação válida
+    combinacoesTotais.push(combinacaoAtual.slice());
+    return;
+  }
+
+  if (somaDesejada < 0 || indiceAtual >= numeros.length) {
+    // fim do array ou a soma ultrapassou o valor 53
+    return;
+  }
+
+  // incluir o elemento atual na combinação
+  combinacaoAtual.push(numeros[indiceAtual]);
+  encontrarCombinacoes(somaDesejada - numeros[indiceAtual], indiceAtual + 1, combinacaoAtual);
+  combinacaoAtual.pop();
+
+  // não incluir o elemento atual na combinação
+  encontrarCombinacoes(somaDesejada, indiceAtual + 1, combinacaoAtual);
+}
+
+encontrarCombinacoes(53, 0, []);
+
+console.log(combinacoesTotais.length);
